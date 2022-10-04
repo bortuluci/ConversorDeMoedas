2f5ba306c8c77a907bf61212f21bce1c1ef94753
Khauan Bortuluci <khauanbs@gmail.com>
2022-10-04 09:55:56 -0300

    First commit

---
+++ UserClass.py
@@ -0,0 +1,152 @@
+import sqlite3
+import requests
+import json
+
+
+conexao=sqlite3.connect('databaseusuarios.db')
+cursor=conexao.cursor()
+
+
+class User:
+  def __init__(self,username=None,password=None,login=0):
+    self.username=username
+    self.password=password
+    self.login=login
+    
+  def novoCadastro(self):
+    print('\n########## CADASTRO ##########')
+    username=str(input('Informe seu usuario para cadastro: '))
+    password=str(input('Digite sua senha para cadastro: '))
+    
+    try:
+      cursor.execute('INSERT INTO usuarios(username,password) VALUES (?,?)',(username,password))
+      conexao.commit()
+      print('Novo usuário cadastrado com sucesso!\n')
+    except:
+      print('Este usuário já existe')
+      self.novoCadastro()
+    
+  def logando(self):
+    print('\n########## LOGIN ##########')
+    
+    username_login=str(input('Username: '))
+    password_login=str(input('Password: '))
+    
+    validacao=cursor.execute(f'SELECT username,password FROM usuarios WHERE username=? AND password=?',(username_login,password_login))
+    
+    i=' '
+    for i in validacao:
+      validacao=i
+      
+    if username_login == i[0] and password_login == i[1]:
+      self.username=username_login
+      self.password=password_login
+      self.login=1 #0= Deslogado // 1= Logado
+      print('Login efetuado.\n')
+      logado = 0
+      
+    else:
+      print('Erro! Login não efetuado.\n')
+  
+  def alterarSenha(self):
+    if self.login == 1:
+      print('\n########## ALTERAR SENHA ##########')
+      
+      senha_atual = str(input('Informe sua senha atual: '))
+      
+      validacao=cursor.execute(f'SELECT username,password FROM usuarios WHERE username=? AND password=?',(self.username,senha_atual))
+      
+      i=' '
+      for i in validacao:
+        validacao=i
+        
+      if self.username == i[0] and senha_atual == i[1]:
+        senha_nova = str(input('Digite a nova senha: '))
+        cursor.execute('UPDATE usuarios SET password = ? WHERE password = ? AND username = ?',(senha_nova,senha_atual,i[0]))
+        self.password = senha_nova
+        conexao.commit()
+        print('Senha alterada com sucesso.\n')
+        
+      else:
+        print('Erro! Dados informados não encontrados.\n')
+        
+    else:
+      print('Você não está logado.')
+  
+  def excluirCadastro(self):
+    if self.login == 1:
+      print('\n########## EXCLUIR CADASTRO ##########')
+      senha_atual=str(input('Informe sua senha atual: '))
+      
+      if self.password == senha_atual:
+        cursor.execute(f'DELETE FROM usuarios WHERE username=? AND password=?',(self.username,senha_atual))
+        conexao.commit()
+        print(f'Cadastro de {self.username} foi excluído com sucesso.\n')
+        self.login = 0
+        self.excluirCadastro()
+               
+      else:
+        print('Erro! A senha informada não confere.\n')
+        return
+        
+    else:
+      print('Você foi deslogado.')
+
+  def principaisMoedas(self):
+    if self.login == 1:
+      print('\n########## PRINCIPAIS MOEDAS ##########\nARS - Peso Argentino\nBRL - Real Brasileiro [REFERÊNCIA]\nBTC - Bitcoin\nCAD = Dólar Canadense\nETH - Ethereum\nEUR - Euro [REFERÊNCIA]\nGBP - Libra Esterlina \nUSD - Dólar Americano [REFERÊNCIA]\n')
+    else:
+      print('Você não está logado.\n') 
+   
+  def converterMoedas(self):
+    if self.login == 1: 
+      moeda1=str(input('Informe a moeda: '))
+      valor1=float(input('Informe o valor a ser convertido: '))
+      moeda2= str(input('Informa a moeda de referência: '))
+      
+      
+      cotacoes = requests.get(f'https://economia.awesomeapi.com.br/last/{moeda1}-{moeda2}')
+      respostaApi=cotacoes.status_code #Retorna 200=OK // 404=Error
+      cotacoes = cotacoes.json()
+      if respostaApi == 200:
+        cotacao_moeda = cotacoes[f'{moeda1.upper()}{moeda2.upper()}']['bid']
+        cotacao_moeda=float(cotacao_moeda) #convertendo de string para float
+        valor_final_moeda = cotacao_moeda*valor1 
+        print(f'1 {moeda1.upper()} = {cotacao_moeda} {moeda2.upper()}')
+        print(f'{valor1} {moeda1.upper()} = {valor_final_moeda} {moeda2.upper()}\n')   
+      else:
+        print('Moedas não reconhecidas.')  
+
+  def visualizarDados(self):
+    if self.login == 1:
+      senha_atual = str(input('Informe sua senha atual: '))
+      
+      validacao=cursor.execute(f'SELECT username,password FROM usuarios WHERE username=? AND password=?',(self.username,senha_atual))
+      
+      i=' '
+      for i in validacao:
+        validacao=i
+
+      if self.username == i[0] and senha_atual == i[1]:
+        print(f'\n########## DADOS DO CADASTRO ##########\nUsername: {self.username}\nPassword: {self.password}\n')
+                
+      else:
+        print('Erro! Dados informados não encontrados\n')
+        
+    else:
+      print('Você não está logado.\n')
+  
+  def deslogar(self):
+    self.login = 0 
+    print('Você saiu da sua conta.\n')
+    return
+
+khauan=User()
+# khauan.novoCadastro()
+# khauan.logando()
+# khauan.alterarSenha()
+# khauan.excluirCadastro()
+# khauan.visualizarDados()
+# khauan.principaisMoedas()
+# khauan.converterMoedas()
+# khauan.deslogar()
---
+++ main.py
@@ -0,0 +1,44 @@
+from UserClass import User
+
+usuarioApp=User()
+  
+logado=0 #0= deslogado // 1= logado
+
+while logado == 0:
+  cadastradoOuNao=int(input('\nVocê já é cadastrado? [1]NÃO / [2]SIM / [0]FECHAR\n'))
+  if cadastradoOuNao == 1:
+    usuarioApp.novoCadastro()
+    
+  elif cadastradoOuNao == 2:
+    usuarioApp.logando()
+    
+    while usuarioApp.login == 1:
+      menu = int(input('\n########## MENU ########## \n [1] Alterar senha. \n [2] Excluir cadastro. \n [3] Principais moedas. \n [4] Converter moedas \n [5] Visualizar dados cadastrados \n [0] Sair\n')) 
+      
+      if menu == 1:
+        usuarioApp.alterarSenha()
+
+      if menu == 2:
+        usuarioApp.excluirCadastro()
+
+      if menu == 3:
+        usuarioApp.principaisMoedas()
+
+      if menu == 4:
+        usuarioApp.converterMoedas()
+
+      if menu == 5:
+        usuarioApp.visualizarDados()
+
+      if menu == 0:
+        usuarioApp.deslogar()
+            
+    else:
+      break
+  elif cadastradoOuNao == 0:
+    break  
+  
+  else:
+    print('Erro! Número não reconhecido.\n')
+else:
+  print('\n########## FIM ##########')
